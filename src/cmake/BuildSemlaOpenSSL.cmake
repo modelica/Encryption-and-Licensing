9ef0fb76107319b9beb1dac11e6ace657055cb10
Axel Mårtensson <axel.martensson@modelon.com>
2024-01-08 15:33:31 +0000

    LIC-770 add cmake variable DOWNLOADED_OPENSSL_SOURCE_URL_HASH_SHA256 to speed up builds.  Improves the build system so that the OpenSSL source code archive is not re-downloaded if it has already been downloaded.

--- src/cmake/BuildSemlaOpenSSL.cmake
+++ src/cmake/BuildSemlaOpenSSL.cmake
@@ -45,11 +45,15 @@ endif()
 if(NOT DOWNLOADED_OPENSSL_SOURCE_URL)
     set(DOWNLOADED_OPENSSL_SOURCE_URL https://www.openssl.org/source/openssl-3.0.8.tar.gz)
 endif()
+if(NOT DOWNLOADED_OPENSSL_SOURCE_URL_HASH_SHA256)
+    set(DOWNLOADED_OPENSSL_SOURCE_URL_HASH_SHA256 6c13d2bf38fdf31eac3ce2a347073673f5d63263398f1f69d0df4a41253e4b3e)
+endif()
 message(STATUS "Will build OpenSSL downloaded from ${DOWNLOADED_OPENSSL_SOURCE_URL} as external project")
 
 ExternalProject_Add(openssl
     PREFIX ${openssl_dir}
     URL ${DOWNLOADED_OPENSSL_SOURCE_URL}
+    URL_HASH SHA256=${DOWNLOADED_OPENSSL_SOURCE_URL_HASH_SHA256}
     TLS_VERIFY true
     NETRC OPTIONAL
     BUILD_IN_SOURCE 1
